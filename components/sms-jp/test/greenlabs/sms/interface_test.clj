f38986f913d3b5f25c63f744c96c4af06f7a93eb
Jungin Kwon <jungin.kwon1@gmail.com>
2022-11-07 03:10:43 +0900

    sms-jp test fixed

--- components/sms-jp/test/greenlabs/sms/interface_test.clj
+++ components/sms-jp/test/greenlabs/sms/interface_test.clj
@@ -1,6 +1,10 @@
 (ns greenlabs.sms.interface-test
-  (:require [clojure.test :as test :refer :all]
+  (:require [clojure.test :as test :refer [deftest is run-tests testing]]
             [greenlabs.sms.interface :as sms]))
 
-(deftest dummy-test
-  (is (= 1 1)))
+(deftest send-verification-code-test
+  (testing "인증 메시지 전송"
+    (is (= "🇯🇵 認証コードは123456です。" (sms/send-verification-code "010-9999-0000" 123456)))))
+
+(comment
+  (run-tests))
